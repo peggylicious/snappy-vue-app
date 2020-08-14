f366673ba8a1976626d06c683c6e900faff49a16
peggylicious <p.asq@live.com>
2020-08-14 22:32:22 +0100

    Add Header and hero

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <snappy-header></snappy-header>
+    <snappy-hero></snappy-hero>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SnappyHeader from './components/SnappyHeader'
+import SnappyHero from './components/SnappyHero'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SnappyHeader,
+    SnappyHero,
   }
 }
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+body{
+  background: #222831;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
